3764d1a9c401f84fabc147398a788d34955a81af
Guruhabc <85942433+Guruhabc@users.noreply.github.com>
2021-12-07 17:00:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+// Create canvas variable
+var canvas = new fabric.Canvas('myCanvas');
+block_y=1;
+block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(get_image)
+{
+   
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+   if(keyPressed == '?') 
+   {
+      RedRanger.jpg
+   }
+   if(keyPressed == '71')
+   {
+       block_x = 200;
+       GreenRanger.png
+   }
+   
+   if(keyPressed == '89')
+   {
+       block_x =350;
+       YellowRanger.png
+   }
+   if(keyPressed == '80')
+   {
+       block_x = 600;
+      PinkRanger.png
+   }
+   if(keyPressed == '66')
+   {
+       block_x = 700;
+      BlueRanger.png
+   }
+   
+}
